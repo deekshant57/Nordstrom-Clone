0d2c7b6f89025e01c61cfbe46933050ba5bc1ac1
epj23Azfqw <89893187+epj23Azfqw@users.noreply.github.com>
2022-01-23 03:32:14 +0530

    kid, men and women
    Kid : category, js, css alert
    men : alert, category js css
    women same as above

--- kid.js
+++ kid.js
@@ -3,7 +3,7 @@ var data = [
       image_url : "https://n.nordstrommedia.com/id/sr3/933a269d-9add-4b2f-bbe1-4d4413960ec5.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name : "Solid Dress Shirt",
       price : 3128,
-      brand: "Robert Barakett",
+      brand: "Topman",
       category : "shirts",
       color : "blue"
     },
@@ -12,7 +12,7 @@ var data = [
       name : "Club Fleece Sweatpants",
       price : 2807,
       brand: "Nike",
-      category : "Sweatpants",
+      category : "Jeans",
       color : "black"
     },
     {
@@ -25,17 +25,17 @@ var data = [
     },
     {
       image_url : "https://n.nordstrommedia.com/id/sr3/968c3f5d-b399-41c7-b943-1cd17c7b9d3d.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",   
-      name : "Two-Piece Jogger Pajamas",
+      name : "Two-Piece Jogger",
       price : 1876,
-      brand: "open edit",
+      brand: "Topman",
       category : "Pajamas",
-      color : "Green"
+      color : "green"
     },
     {
       image_url : "https://n.nordstrommedia.com/id/sr3/e84f03b7-3bb2-4d50-910d-a61bbd8a016a.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",     
       name : "Brixton Stretch Jeans",
       price :  2005.25,
-      brand: "JOE'S",
+      brand: "Topman",
       category : "Jeans",
       color : "Rinse"
     },
@@ -43,7 +43,7 @@ var data = [
       image_url : "https://n.nordstrommedia.com/id/sr3/3e1eb47f-0a3e-4809-9628-85bd979bc062.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",      
       name : "Premium Fleece Hoodie",
       price : 4010.50,
-      brand: "CHAMPION",
+      brand: "Topman",
       category : "Hoodie",
       color : "Black"
     },
@@ -51,8 +51,8 @@ var data = [
       image_url : "https://n.nordstrommedia.com/id/sr3/586f170d-0562-44c8-9708-801cf304e88a.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name : "Kids' Graphic Tee",
       price : 1058,
-      brand: "QUIKSILVER",
-      category : "T-shirt",
+      brand: "Topman",
+      category : "shirts",
       color : "Blue"
     },
     {
@@ -65,41 +65,41 @@ var data = [
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/2d4a5583-ce2a-4cec-b1d9-b6f7c141e5f6.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Digital Camo Athletic Shorts",
+      name: "Digital Camo Athletic",
       price: 2807.35,
-      brand: "NIKE",
+      brand: "Nike",
       category: "Shorts",
       color: "Black"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/4fbe3358-1ab8-4933-955a-d9db6a944ad1.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Stretch Stripe Swim Trunks",
+      name: "Stretch Stripe",
       price: 2807.35,
-      brand: "O'NEILL",
+      brand: "Nike",
       category: "Swim Trunks",
       color: "pink"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/c8e3b92f-c6d7-4c0a-b278-c44dd6bd6890.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Regular Taper Jeans",
+      name: "Regular Taper",
       price: 2405.49,
-      brand: "LEVI'S®",
+      brand: "Nike",
       category: "Jeans",
       color: "Sharkley"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/fb94d76e-674d-4046-b3f1-bd7296c5269b.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Flannel Button-Up Shirt",
+      name: "Flannel Button-Up",
       price: 3368.82,
-      brand: "VANS",
+      brand: "Nike",
       category: "shirts",
       color: "black"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/6b325def-4480-4ec1-86e7-74895adb56e7.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Knit Wool Puffer Jacket",
+      name: "Knit Wool Puffer",
       price: 36094.46,
-      brand: "CANADA GOOSE",
+      brand: "Nike",
       category: "Jacket",
       color: "white"
     },
@@ -107,15 +107,15 @@ var data = [
       image_url: "https://n.nordstrommedia.com/id/sr3/5ed41b74-540a-4b34-bd42-6b22bf6df396.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name: " High Pile Fleece Jacket",
       price:  7940.78,
-      brand: "THE NORTH FACE",
-      category: "Fleece Jacket",
+      brand: "Nike",
+      category: "Jacket",
       color: "Summit Gold/ Blue/ FlarE"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/e4b1df8d-79ae-4b52-a0ea-ca62deb50f8b.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Sportswear Tech Zip Hoodie",
+      name: "Sportswear Tech",
       price: 7619.94,
-      brand: "NIKE",
+      brand: "Nike",
       category: "Hoodie",
       color: "Orange"
     },
@@ -123,7 +123,7 @@ var data = [
       image_url: "https://n.nordstrommedia.com/id/sr3/85df59ef-4880-4a5e-8393-74bf7f543422.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name: "Two-Piece Pajama Set",
       price: 1684,
-      brand: "TREASURE & BOND",
+      brand: "Nike",
       category: "Pajama",
       color: "Black"
     },
@@ -131,31 +131,31 @@ var data = [
       image_url: "https://n.nordstrommedia.com/id/sr3/b89662dd-f060-4a42-9619-41d0b3dea016.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name: "Clear Front Masks",
       price:  1840.82,
-      brand: "ANDY & EVAN",
+      brand: "Nordstrom",
       category: "Masks",
       color: "black"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/67d788a6-a13e-44ac-8cba-520433b39efd.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Organic Cotton Bodysuit",
+      name: "Organic Cotton",
       price: 2566.72,
-      brand: "TENTH & PINE",
-      category: "Bodysuit",
+      brand: "Nordstrom",
+      category: "Pajamas",
       color: "Natural"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/7c811324-2ab2-482c-b1a6-53fca7169913.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Two-Piece Organic Cotton Pajamas",
+      name: "Two-Piece Organic",
       price: 1636,
-      brand: "Daniel Buchler",
-      category: "PETIT LEM",
+      brand: "Nordstrom",
+      category: "Pajamas",
       color: "black"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/2c6559ae-7bf2-4faf-8a8e-9954a7882ede.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name: "Cotton One-Piece Pajamas",
       price:  3047.98,
-      brand: "PEHR",
+      brand: "Nordstrom",
       category: "Pajamas",
       color: "White"
     },
@@ -163,7 +163,7 @@ var data = [
       image_url: "https://n.nordstrommedia.com/id/sr3/f095c4c1-6372-484a-9164-7c50345b3755.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name: "Comfort Waffle Robe",
       price: 6523,
-      brand: "L.L.Bean",
+      brand: "Nordstrom",
       category: "Loungewear",
       color: "blue"
     },
@@ -171,13 +171,13 @@ var data = [
       image_url: "https://n.nordstrommedia.com/id/sr3/f7d2a213-b1a8-404c-b612-fd174dcf9732.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name: "Flower Cotton Bathrobe",
       price: 1236,
-      brand: "Dusen Dusen",
+      brand: "Nordstrom",
       category: "Loungewear",
       color: "white"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/63c0913e-5ee1-4478-a512-36f093908ffc.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Cashmere Crewneck Sweater",
+      name: "Cashmere Crewneck",
       price: 7885,
       brand: "Nordstrom",
       category: "sweaters",
@@ -185,7 +185,7 @@ var data = [
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/c400f39c-733e-498f-ad3c-97fe4b922a13.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: " Fit Cashmere Quarter Zip Pullover",
+      name: " Fit Cashmere",
       price: 5486,
       brand: "Nordstrom",
       category: "sweaters",
@@ -203,37 +203,37 @@ var data = [
       image_url: "https://n.nordstrommedia.com/id/sr3/31e5b1b1-0a84-4d03-b219-6c85b14c10be.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
       name: "Merrick Bay Sweater",
       price: 4563,
-      brand: "Rodd & Gunn",
+      brand: "Nordstrom",
       category: "sweaters",
       color: "black"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/2680ad74-7280-4a6f-af61-d493cfbd52f4.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Solid Turtleneck Sweater",
+      name: "Solid Turtleneck",
       price: 4589,
-      brand: "Topman",
+      brand: "Polo",
       category: "sweaters",
       color: "orange"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/4da8598f-26eb-4630-86c4-5c1a5d37d38c.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Fit Quarter Zip Pullover",
+      name: "Fit Quarter Zip",
       price: 5125,
-      brand: "Topman",
+      brand: "Polo",
       category: "sweaters",
       color: "maroon"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/99a672b7-8062-4289-83eb-abb9af799a2f.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Mode Slim Fit Wool Sweater",
+      name: "Mode Slim Fit",
       price: 4566,
-      brand: "AllSaints",
+      brand: "Polo",
       category: "sweaters",
       color: "green"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/ffdc6be5-a69d-4a53-8972-84df54c03fe3.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Cotton & Cashmere Pullover",
+      name: "Cotton & Cashmere",
       price: 1123,
       brand: "Nordstrom",
       category: "sweaters",
@@ -241,23 +241,23 @@ var data = [
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/63beab63-d262-4315-9f91-1594d4918eb7.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Texture Crewneck Sweater",
+      name: "Texture Crewneck",
       price: 4456,
-      brand: "Treasure & Bond",
+      brand: "Polo",
       category: "sweaters",
       color: "white"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/b3dbbac5-4c20-492c-b960-9d0ee08194ad.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Wool Quarter Zip Sweater",
+      name: "Wool Quarter Zip",
       price: 4455,
-      brand: "Reiss",
+      brand: "Polo",
       category: "sweaters",
       color: "maroon"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/e6f0fa4a-d7af-4049-99b4-65cee9f4e62b.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Essential Slim Fit Jeans",
+      name: "Essential Slim Fit",
       price: 4444,
       brand: "Topman",
       category: "Jeans",
@@ -273,7 +273,7 @@ var data = [
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/9ca4a489-89ed-409a-8a1f-621c355fbb5e.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Essential Slim Fit Jeans",
+      name: "Essential Slim Fit",
       price: 11223,
       brand: "Topman",
       category: "Jeans",
@@ -297,7 +297,7 @@ var data = [
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/b5d925db-e546-4562-a516-ddc2e23dedfc.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Ripped Stretch Skinny Jeans",
+      name: "Ripped Stretch",
       price: 4562,
       brand: "Topman",
       category: "Jeans",
@@ -305,9 +305,9 @@ var data = [
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/0e247cf7-26fe-4915-b99c-4a9febcc2657.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Fit Performance Stretch Jeans",
+      name: "Fit Performance",
       price: 5687,
-      brand: "Devil-Dog Dungarees",
+      brand: "Topman",
       category: "Jeans",
       color: "blue"
     },
@@ -321,58 +321,58 @@ var data = [
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/724761e8-4c6b-48e2-9426-b877eb8b4f55.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Zach Straight Leg Jeans ",
+      name: "Zach Straight",
       price: 1235,
-      brand: "Mavi Jeans",
+      brand: "Topman",
       category: "Jeans",
       color: "blue"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/0a65d941-2682-4e3a-bb0e-3a58f874adc4.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Transcend Lennox Slim Fit Jeans",
+      name: "Slim Fit Jeans",
       price: 7777,
-      brand: "PAIGE",
+      brand: "Polo",
       category: "Jeans",
       color: "black"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/ca16d0e4-ed7c-4748-9698-6069a947f22c.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Icon Bomber Jacket",
+      name: "Icon Bomber",
       price: 4568,
       brand: "Topman",
-      category: "Jackets",
+      category: "Jacket",
       color: "green"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/b549c913-e044-4f62-9ef4-668c1ef4a495.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Water-Resistant Car Coat",
+      name: "Water-Resistant",
       price: 7456,
-      brand: "Peter Millar",
-      category: "Jackets",
+      brand: "Polo",
+      category: "Jacket",
       color: "blue"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/dcb44bd1-ac55-4d8f-b138-1b5a6327e022.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: " Millerton Waterproof Hooded Jacket",
+      name: " Millerton Waterproof",
       price: 7456,
-      brand: "The North Face",
-      category: "Jackets",
+      brand: "Polo",
+      category: "Jacket",
       color: "balck"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/32ce084f-1e28-43f4-bd0c-08b15be9da8b.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: " Classic Fit Denim Jacket",
+      name: " Classic Fit Denim",
       price: 1245,
       brand: "Topman",
-      category: "Jackets",
+      category: "Jacket",
       color: "blue"
     },
     {
       image_url: "https://n.nordstrommedia.com/id/sr3/8c5b0115-d425-4547-9949-b0b875b20f57.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
-      name: "Packable Quilted Down Jacket",
+      name: "Packable Quilted",
       price: 7778,
-      brand: "The North Face",
-      category: "Jackets",
+      brand: "Polo",
+      category: "Jacket",
       color: "red"
     }
   ];
@@ -458,7 +458,7 @@ var data = [
          if(chbox5.checked==true)
          {
              var filterlist =data.filter(function(elem){
-                 return elem.category == "Jackets"
+                 return elem.category == "Jacket"
              })
              console.log(filterlist)
              display(filterlist)
@@ -662,6 +662,75 @@ var data = [
          }
      })
 
+     var chbox17 = document.querySelector("#flexCheckDefault17")
+     chbox17.addEventListener("change",function()
+     {
+        document.querySelector("#blockelem").innerHTML=""
+         console.log(chbox17.checked)
+         if(chbox17.checked==true)
+         {
+             var filterlist =data.filter(function(elem){
+                 return elem.brand == "Topman"
+             })
+             console.log(filterlist)
+             display(filterlist)
+         }
+         else{
+             display(data)
+         }
+     })
+     var chbox18 = document.querySelector("#flexCheckDefault18")
+     chbox18.addEventListener("change",function()
+     {
+        document.querySelector("#blockelem").innerHTML=""
+         console.log(chbox18.checked)
+         if(chbox18.checked==true)
+         {
+             var filterlist =data.filter(function(elem){
+                 return elem.brand == "Nike"
+             })
+             console.log(filterlist)
+             display(filterlist)
+         }
+         else{
+             display(data)
+         }
+     })
+     var chbox19 = document.querySelector("#flexCheckDefault19")
+     chbox19.addEventListener("change",function()
+     {
+        document.querySelector("#blockelem").innerHTML=""
+         console.log(chbox19.checked)
+         if(chbox19.checked==true)
+         {
+             var filterlist =data.filter(function(elem){
+                 return elem.brand == "Nordstrom"
+             })
+             console.log(filterlist)
+             display(filterlist)
+         }
+         else{
+             display(data)
+         }
+     })
+     var chbox20 = document.querySelector("#flexCheckDefault20")
+     chbox20.addEventListener("change",function()
+     {
+        document.querySelector("#blockelem").innerHTML=""
+         console.log(chbox20.checked)
+         if(chbox20.checked==true)
+         {
+             var filterlist =data.filter(function(elem){
+                 return elem.brand == "Polo"
+             })
+             console.log(filterlist)
+             display(filterlist)
+         }
+         else{
+             display(data)
+         }
+     })
+
 
      var chbox23 = document.querySelector("#flexCheckDefault44")
      chbox23.addEventListener("change",function()
@@ -760,22 +829,46 @@ var data = [
         div2.append(btn,wishList);
 
         div2.setAttribute("class","bagAndwishlist")
+        var alrtdiv = document.createElement("div");
+    alrtdiv.classList.add("d-none", "alert", "alert-success");
+    alrtdiv.setAttribute("role", "alert");
+    // alrtdiv.innerHTML = "<div>Added Successfully</div>";
+    alrtdiv.textContent = "Added Successfully";
 
-        div.append(avatar,h4,h5,cost,div2); // ProductImage,ProductBrand,ProductName,ProductCost,Bag&wishlist
+    var alrtclosebtn = document.createElement("button");
+    alrtclosebtn.setAttribute("type", "button");
+    alrtclosebtn.setAttribute("data-bs-dismiss", "alert");
+    alrtclosebtn.setAttribute("aria-label", "Close");
+    alrtclosebtn.classList.add("btn-close", "ms-1");
+    alrtdiv.append(alrtclosebtn);
+    alrtclosebtn.addEventListener("click", function () {
+      alrtdiv.classList.add("d-none");
+      // alrtdiv.style.display = "none";
+      console.log("object");
+    });
+        div.append(avatar,h4,h5,cost,div2,alrtdiv); // ProductImage,ProductBrand,ProductName,ProductCost,Bag&wishlist
         document.querySelector("#blockelem").append(div);
+        function addtobag(elem)
+        {
+        cartarr.push(elem)
+        console.log(cartarr);
+        alrtdiv.classList.remove("d-none");
+        // document.querySelector("#alret").classList.remove("d-none")
+    
+        localStorage.setItem("cartitem",JSON.stringify(cartarr));
+   
+    
+    }
     });
   }
     
-    function addtobag(elem)
-    {
-        cartarr.push(elem)
-        console.log();
-        document.querySelector("#alret").classList.remove("d-none")
-    
-    localStorage.setItem("cartitem",JSON.stringify(cartarr));
-   
-    
-    };
+    ;
+    function addtowishlist(elem) {
+  wishlistarr.push(elem);
+  console.log(wishlistarr);
+  
+  localStorage.setItem("wishlist", JSON.stringify(wishlistarr));
+}
 
     // function sortprice() {
     // // document.querySelector("#blockelem").textContent = "";
