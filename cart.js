2f372097c036e51cc006ca130ab5d17582088c9b
deekshant57 <deekshant57@gmail.com>
2022-02-17 22:35:14 +0530

    Revert "Merge branch 'main' of https://github.com/deekshant57/Nordstrom-Clone"
    This reverts commit f47b8bde99716295fd57b49c7135707feafd6d08, reversing
    changes made to fe7eef7a92ff4540695e75fda389bf4d57a3c8ea.

--- cart.js
+++ cart.js
@@ -1,75 +1,72 @@
-var cartarr = JSON.parse(localStorage.getItem("cartitem")) || [];
-discart(cartarr);
-function discart(cartarr) {
-  cartarr.map(function (elem, index) {
-    var div1 = document.createElement("div");
-    div1.setAttribute("id", "box1");
-    var div2 = document.createElement("div");
-    var avatar = document.createElement("img");
-    avatar.setAttribute("src", elem.image_url);
-    avatar.setAttribute("id", "avatar");
-    var h4 = document.createElement("h6");
-    h4.textContent = elem.brand;
-    var h5 = document.createElement("p");
-    h5.textContent = elem.name;
-    var btn = document.createElement("button");
-    btn.textContent = "";
-    btn.setAttribute("id", "deltebtn");
-    btn.addEventListener("click", function () {
-      delcart(index);
-    });
-    var i = document.createElement("i");
-    i.classList.add("bi", "bi-trash-fill");
-    btn.append(i);
-    //     var cost = document.createElement("h5");
-    // cost.textContent = `INR ${elem.price}`;
-    // cost.setAttribute("class", "Productcost")
-    var cost = document.createElement("b");
-    cost.textContent = `INR ${elem.price}`;
-    // var div3 = document.createElement("div");
-    // div3.setAttribute("id", "btndel")
-    div2.append(h4, h5, cost, btn);
-    div1.append(avatar, div2);
-    document.querySelector("#blockelem").append(div1);
-  });
-}
-var total = cartarr.reduce(function (acc, cv) {
-  return acc + cv.price;
-}, 0);
-console.log(total);
-document.querySelector("#total").textContent = total;
-var x = total - (total * 30) / 100;
-localStorage.setItem("total", JSON.stringify(x));
+var cartarr = JSON.parse(localStorage.getItem("cartitem")) || []
+    discart(cartarr)
+    function discart(cartarr) {
+        cartarr.map(function (elem, index) {
+            var div1 = document.createElement("div")
+            div1.setAttribute("id", "box1")
+            var div2 = document.createElement("div")
+            var avatar = document.createElement("img");
+            avatar.setAttribute("src", elem.image_url);
+            avatar.setAttribute("id", "avatar");
+            var h4 = document.createElement("h6");
+            h4.textContent = elem.brand;
+            var h5 = document.createElement("p");
+            h5.textContent = elem.name;
+            var btn = document.createElement("button");
+            btn.textContent = "";
+            btn.setAttribute("id", "deltebtn")
+            btn.addEventListener("click", function () {
+                delcart(index)
+            });
+            var i = document.createElement("i");
+            i.classList.add("bi", "bi-trash-fill");
+            btn.append(i);
+        //     var cost = document.createElement("h5");
+        // cost.textContent = `INR ${elem.price}`;
+        // cost.setAttribute("class", "Productcost")
+            var cost = document.createElement("b");
+            cost.textContent = `INR ${elem.price}`;
+            // var div3 = document.createElement("div");
+            // div3.setAttribute("id", "btndel")
+            div2.append(h4, h5, cost, btn)
+            div1.append(avatar, div2)
+            document.querySelector("#blockelem").append(div1);
+        });
+    }
+    var total = cartarr.reduce(function (acc, cv) {
+        return acc + cv.price
+    }, 0)
+    console.log(total)
+    document.querySelector("#total").textContent = total
+    var x = total-(total*30/100) 
+   
+    document.querySelector("#ta").textContent = x
+    function delcart(index) {
+        cartarr.splice(index, 1)
+        localStorage.setItem("cartitem", JSON.stringify(cartarr))
+        discart(cartarr)
+        location.reload()
+    }
+    document.querySelector("form").addEventListener("submit", coupon)
+    function coupon() {
+        event.preventDefault();
+        var val = document.querySelector("#coupon").value
+        console.log(val)
+        if (val == "GIFT4YOU") {
+            // var alret = document.createElement("div");
+            //     alret.setAttribute("id","alert");
+            document.querySelector("#appcoup").textContent = "Coupon Applied"
+            console.log(x)
+            var y = x - (x/10)
+            document.querySelector("#ta").textContent = y
 
-document.querySelector("#ta").textContent = x;
-function delcart(index) {
-  cartarr.splice(index, 1);
-  localStorage.setItem("cartitem", JSON.stringify(cartarr));
-  discart(cartarr);
-  location.reload();
-}
-document.querySelector("form").addEventListener("submit", coupon);
-function coupon() {
-  event.preventDefault();
-  var val = document.querySelector("#coupon").value;
-  console.log(val);
-  if (val == "GIFT4YOU") {
-    // var alret = document.createElement("div");
-    //     alret.setAttribute("id","alert");
-    document.querySelector("#appcoup").textContent = "Coupon Applied";
-    console.log(x);
-    var y = Math.ceil(x - x / 10);
-    document.querySelector("#ta").textContent = y;
-    localStorage.setItem("total", JSON.stringify(y));
-
-    alert("Coupon Applied");
-  } else {
-    alert("Invalid Coupon");
-  }
-}
-function removeall() {
-  document.querySelector("#blockelem").textContent = "";
-}
-var length = cartarr.length;
-console.log(length);
-localStorage.setItem("length", JSON.stringify(length));
+            alert("Coupon Applied")
+        }
+        else {
+            alert("Invalid Coupon")
+        }
+    }
+    function removeall(){
+        document.querySelector("#blockelem").textContent = "";
+                
+        }
